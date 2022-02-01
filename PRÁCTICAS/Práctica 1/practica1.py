3e82ec68c78e1211d319b7fcfd6bf781afc3d3c4
Noelia Arroyo <78790368+narroyoc96@users.noreply.github.com>
2022-02-01 19:49:02 +0100

    Add files via upload

---
+++ PRÁCTICAS/Práctica 1/practica1.py
@@ -0,0 +1,25 @@
+
+#Noelia Arroyo Castaño
+#Ejercicio1_Práctica1
+
+#numero entero
+a=10
+print(a)
+
+#numero real
+b=3.14
+print(b)
+
+#cadena de caracteres
+nombre = ["n" "o" "e" "l" "i" "a"]
+print(nombre)
+
+#lista
+asignaturas = ["STMA", "PTAVI", "CIA", "TDS"]
+print(asignaturas)
+
+
+#lista asignatura pendientes
+pendiente = ["STMA", "PTAVI", "CIA", "TDS", "TDI", "ECAV", "3D", "ASA"]
+N = len(pendiente)
+print("Me quedan " + str(N) + " asignaturas para acabar la carrera. Son " + str(pendiente) + " Buff, ¡qué ganas!")
